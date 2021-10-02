8093ecd63d8994cf3978b0e6f301c540f6ba1ecf
Lenora Esquenazi <lenaciousd@gmail.com>
2021-10-01 17:27:41 -0700

    Implemented getAnswers route and started post questions route

--- server/index.js
+++ server/index.js
@@ -20,36 +20,34 @@ app.get('/qa/questions', (req, res) => {
 });
 
 app.get('/qa/questions/:question_id/answers', (req, res) => {
-  // db.getAnswers(req, (error, data) => {
-  //   if (error) {
-  //     console.log('ERROR in server GET answers request ', error);
-  //     res.status(404).send('unable to access database');
-  //   } else {
-  // res.status(200).send(data);
-  // res.json({ endpoint: 'list of answers for 1 question' });
-  //   }
-  // });
+  db.getAnswers(req, (error, data) => {
+    if (error) {
+      console.log('ERROR in server GET answers request ', error);
+      res.json('unable to access database');
+    } else {
+      res.json(data);
+    }
+  });
 });
 
 app.post('/qa/questions', (req, res) => {
-  // db.postQuestions(req, (error, data) => {
-  //   if (error) {
-  //     console.log('ERROR in server POST questions request ', error);
-  //     res.status(404).send('unable to post to database');
-  //   } else {
-  // res.status(200).send(data);
-  //   res.json({ endpoint: 'post a question for 1 product' });
-  //   }
-  // });
+  db.postQuestions(req, (error, data) => {
+    if (error) {
+      console.log('ERROR in server POST questions request ', error);
+      res.json('unable to post to database');
+    } else {
+      res.json(data);
+    }
+  });
 });
 
 app.post('/qa/questions/:question_id/answers', (req, res) => {
   // db.postAnswers(req, (error, data) => {
   //   if (error) {
   //     console.log('ERROR in server POST answers request ', error);
-  //     res.status(404).send('unable to post to database');
+  //     res.json('unable to post to database');
   //   } else {
-  // res.status(200).send(data);
+  // res.json(data);
   // res.json({ endpoint: 'post an answer for 1 question' });
   //   }
   // });
@@ -59,9 +57,9 @@ app.put('/qa/questions/:question_id/helpful', (req, res) => {
   // db.putQuestionsHelpful(req, (error, data) => {
   //   if (error) {
   //     console.log('ERROR in server put questions helpful request ', error);
-  //     res.status(404).send('unable to access database');
+  //     res.json('unable to access database');
   //   } else {
-  // res.status(200).send(data);
+  // res.json(data);
   // res.json({ endpoint: 'mark question helpful' });
   //   }
   // });
@@ -71,9 +69,9 @@ app.put('/qa/questions/:question_id/report', (req, res) => {
   // db.putQuestionsReport(req, (error, data) => {
   //   if (error) {
   //     console.log('ERROR in server put questions report request ', error);
-  //     res.status(404).send('unable to access database');
+  //     res.json('unable to access database');
   //   } else {
-  // res.status(200).send(data);
+  // res.json(data);
   // res.json({ endpoint: 'report question' });
   //   }
   // });
@@ -83,9 +81,9 @@ app.put('/qa/answers/:answer_id/helpful', (req, res) => {
   // db.putAnswersHelpful(req, (error, data) => {
   //   if (error) {
   //     console.log('ERROR in server put answers helpful request ', error);
-  //     res.status(404).send('unable to access database');
+  //     res.json('unable to access database');
   //   } else {
-  // res.status(200).send(data);
+  // res.json(data);
   // res.json({ endpoint: 'mark answer helpful' });
   //   }
   // });
@@ -95,9 +93,9 @@ app.put('/qa/answers/:answer_id/report', (req, res) => {
   // db.putAnswersReport(req, (error, data) => {
   //   if (error) {
   //     console.log('ERROR in server put answers report request ', error);
-  //     res.status(404).send('unable to access database');
+  //     res.json('unable to access database');
   //   } else {
-  // res.status(200).send(data);
+  // res.json(data);
   // res.json({ endpoint: 'report answer' });
   //   }
   // });
