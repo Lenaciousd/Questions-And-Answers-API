804276272ef17641f28290cabc8f8f9090c2f417
Lenora Esquenazi <lenaciousd@gmail.com>
2021-10-01 17:29:12 -0700

    Updated query for getQuestions, added query for getAnswers. Still need to add query string to postQuestions

--- database/index.js
+++ database/index.js
@@ -9,7 +9,19 @@ const pool = new Pool({
 });
 
 const getQuestions = (req, callback) => {
-  pool.query('SELECT * FROM questions WHERE id=$1', [40351])
+  pool.query('(SELECT json_agg(results) FROM (SELECT * FROM questions WHERE product_id=$1) results)', [40351])
+    .then((data) => {
+      console.log('DATA:', data);
+      callback(null, data.rows);
+    })
+    .catch((error) => {
+      console.log('ERROR:', error);
+      callback(error, null);
+    });
+};
+
+const getAnswers = (req, callback) => {
+  pool.query('(SELECT json_agg(results) FROM(SELECT * FROM answers WHERE question_id=$1) results)', [28])
     .then((data) => {
       console.log('DATA:', data);
       callback(null, data);
@@ -20,23 +32,27 @@ const getQuestions = (req, callback) => {
     });
 };
 
-const getAnswers = () => {
+const postQuestions = (req, callback) => {
+  pool.query('(INSERT INTO questions VALUES ())', [])
+    .then((data) => {
+      console.log('DATA:', data);
+      callback(null, data);
+    })
+    .catch((error) => {
+      console.log('ERROR:', error);
+      callback(error, null);
+    });
+};
+
+const postAnswers = (req, callback) => {
 
 };
 
-const postQuestions = () => {
+const putQuestionsHelpful = (req, callback) => {
 
 };
 
-const postAnswers = () => {
-
-};
-
-const putQuestionsHelpful = () => {
-
-};
-
-const putQuestionsReport = () => {
+const putQuestionsReport = (req, callback) => {
 
 };
 
